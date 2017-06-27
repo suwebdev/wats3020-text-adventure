09c0773a0016a56d1efab72b1e67e88565bd46d0
Shawn Rider <shawn@shawnrider.com>
2017-06-26 18:03:55 -0700

    Initial commit.
    Change-Id: I16888894518c8cb8f5807dc500bf8892fa9a3633

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# wats3020-text-adventure
-A JavaScript exercise aimed at organizing code using functions.
+# WATS 3020 Text Adventure
+
+In this project, you will be asked to create a simple text adventure game that
+allows a user to make choices that influence the outcome. To accomplish this
+goal, you will use functions that can execute whenever an Event is triggered.
+(Note: You will not be required to create the Events.)
+
+Here is the interaction that we're going for:
+
+1. User arrives on page, sees first page of story.
+2. User selects a choice.
+3. User sees the next page based on their choice.
+4. User continues to make choices on each page until they reach an ending to the story.
+
+This fits the typical "choose-your-own-adventure" template, so if you're
+familiar with those kinds of stories, then you should be able to imagine what
+this will look like.
+
+## Basic Requirements
+In order to complete this project successfully, you will need to fulfill these
+requirements:
+
+* Add a function to prompt the user for a character name.
+* Add a function to retreive the next chunk of text.
+* Add a function to "undo" choices the player has made.
+* Add a function to restart the game.
+
+
+## Stretch Goals
+If this project is too easy, attempt these stretch goals to make it more
+challenging.
+
+* Enhance the project with an original story, or a more ambitious story, or one that explores allowing for more than two choices. (You might consult some resources like ["How to Write Your Own Choose Your Own Adventure Story"](http://blog.karenwoodward.org/2014/06/how-to-write-choose-your-own-adventure.html) by Karen Woodward, or ["Designing Branching Narratives"](https://thestoryelement.wordpress.com/2015/02/11/designing-branching-narrative/) by Paul Nelson.)
+* In your new story you could add additional characters that the User could name. Write functions to prompt the user for those names and then manage their insertion into the text in appropriate places.
---
+++ js/main.js
@@ -0,0 +1,246 @@
+/* JS for WATS 3020 Text Adventure */
+let playerName = "";
+let choiceList = [];
+let currentPage = null;
+
+///////////////////////////////////////////////////
+//////// TODOs ///////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// Fill in the blanks below to complete each TODO task.                       //
+////////////////////////////////////////////////////////////////////////////////
+
+// TODO: Prompt the user for their name. Store the name in the variable `playerName`.
+
+// TODO: Create a function called `getCurrentPage()`. It should accept one
+// parameter, which is the `slug` for the current page. This function will fetch
+// the current page and return a page object.
+
+// TODO: Create a function called `recordChoice()` that will accept a `slug`
+// parameter and store it in the `choiceList` Array.
+
+// TODO: Create a function called `undoChoice()` that will remove the last
+// `slug` in the `choiceList` Array and will return a page object.
+
+
+///////////////////////////////////////////////////
+//////// Story Data //////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// Only edit this data to change/enhance the story. Be sure to watch for how  //
+// changes to the story data might affect the mechanisms that output the      //
+// story.                                                                     //
+////////////////////////////////////////////////////////////////////////////////
+// NOTE: The data below is organized as a JS Object. The content for each     //
+// page is stored using a "slug" -- a short alphanumeric identifier (for      //
+// example, "p1", "p2", "homeEnd", etc.). Each page contains a `text`         //
+// property and a `choices` property. The `choices` property is an Array that //
+// contains all of the choices, including the slug that each choice should    //
+// link to.                                                                   //
+////////////////////////////////////////////////////////////////////////////////
+
+var storyData = {
+    title: "The Crow and the Fox",
+    p1: {
+        text: `You are a crow named ${playerName}. You are flying high above the
+                countryside. You see a farm off to the West, and your home forest
+                off to the East.`,
+        choices: [
+            {
+                text: `Fly over the farm to the West.`,
+                link: 'p2'
+            }, {
+                text: `Fly back home to your nest in the forest.`,
+                link: 'homeEnd'
+            }
+        ]
+    },
+    homeEnd : {
+        text: `You return home to your comfy roost in the forest canopy and
+                enjoy a hot cup of tea!
+                <br><br>
+                The End.`,
+        choices: [
+            {
+                text: `Play again?`,
+                link: 'p1'
+            }
+        ]
+    },
+    p2 : {
+        text: `You fly over the Farm and see a piece of cheese lying on the
+                picnic table. There are no people around that you can see. The
+                cheese looks very tasty, but you are worried there might be a
+                person or, even worse, a CAT lurking somewhere you can't see.`,
+        choices: [
+            {
+                text: `Go for the cheese!`,
+                link: 'p3'
+            }, {
+                text: `Decide it's not worth the risk and fly back to the forest.`,
+                link: 'homeEnd'
+            }
+        ]
+    },
+    p3 : {
+        text: `You swoop down and pluck the cheese from the table. Just as you
+                grab hold of the cheese, the farmer's cat leaps onto the table
+                ahead of you!`,
+        choices: [
+            {
+                text: `Veer off to the left trying to avoid the cat.`,
+                link: 'basketEnd'
+            }, {
+                text: `Fly directly at the cat, full steam ahead!`,
+                link: 'p4'
+            }
+        ]
+    },
+    basketEnd : {
+        text: `You fly directly into a picnic basket, which slams shut behind you.
+                You are stuck until some kind human comes to open the basket.
+                But at least the cat didn't eat you!
+                <br><br>
+                The End`,
+        choices: [
+            {
+                text: `Start over?`,
+                link: 'p1'
+            }
+        ]
+    },
+    p4 : {
+        text: `You zoom towards the cat, who is surprised by the direct approach
+                and leaps off the table. You pull up sharply and make it over the
+                big oak tree to a safe cruising altitude. The sun is shining,
+                the wind is beneath your wings, and you have a beak full of
+                cheese.`,
+        choices: [
+            {
+                text: `Find somewhere nice to eat your cheese.`,
+                link: 'p5'
+            }
+        ]
+    },
+    p5 : {
+        text: `You find a secluded fence post in the middle of a large field
+                full of wildflowers. You decide this will be a wonderful place
+                to have a snack.
+                <br><br>
+                Just as you settle down you see Mr. Fox strolling down the path
+                towards your fence post.`,
+        choices: [
+            {
+                text: `Say, "Hello Mr. Fox! Join me for cheese."`,
+                link: 'shareCheese'
+            }, {
+                text: `Keep a wary eye on Mr. Fox.`,
+                link: 'p6'
+            }
+        ]
+    },
+    shareCheese : {
+        text: `You hop down to the ground and Mr. Fox helps you break the cheese
+                in half. He is very grateful to you for sharing your cheese, and
+                he gives you a lovely ribbon for your nest.
+                <br><br>
+                The End`,
+        choices: [
+            {
+                text: `Start over?`,
+                link: 'p1'
+            }
+        ]
+    },
+    p6 : {
+        text: `Mr. Fox approaches and says, "Hello ${playerName}! It's been so
+                long since we've seen each other. I've missed hearing your
+                lovely singing voice. Won't you sing me a tune before I go?`,
+        choices: [
+            {
+                text: `Sing a song for Mr. Fox.`,
+                link: 'dropCheeseEnd'
+            }, {
+                text: `Remain silent.`,
+                link: 'p7'
+            }
+        ]
+    },
+    dropCheeseEnd : {
+        text: `You open your beak to sing a lovely song, and your cheese comes
+                tumbling out. Mr. Fox quickly snaps the cheese out of the air
+                as it falls and gobbles it up!
+                <br><br>
+                The End`,
+        choices: [
+            {
+                text: `Start over?`,
+                link: 'p1'
+            }
+        ]
+    },
+    p7 : {
+        text: `You remain silent through all of Mr. Fox's flattery. In the end,
+                he knows you won't fall for his tricks, and he leaves you alone.
+                <br><br>
+                Finally able to relax in quiet, you enjoy your well-earned
+                cheese.
+                <br><br>
+                The End`,
+        choices: [
+            {
+                text: `Play again?`,
+                link: 'p1'
+            }
+        ]
+    }
+};
+
+///////////////////////////////////////////////////
+//////// Main Script /////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// This script runs the game. You should only edit it if you are attempting a //
+// stretch goal. Otherwise, this script calls the functions that you have     //
+// created above.                                                             //
+////////////////////////////////////////////////////////////////////////////////
+
+let title = document.querySelector('#story-title');
+title.innerHTML = storyData.title;
+
+let pageContent = document.querySelector('#story-text');
+let choicesUL = document.querySelector('#choices');
+
+function updatePage(page) {
+    pageContent.innerHTML = page.text;
+    choicesUL.innerHTML = '';
+    for (choice of page.choices){
+        let newLI = document.createElement('li');
+        newLI.innerHTML = choice.text;
+        newLI.setAttribute('data-slug', choice.link);
+        choicesUL.appendChild(newLI);
+    }
+}
+
+function addEventListeners(){
+    let choices = document.querySelectorAll('#choices li');
+    for (choice of choices){
+        choice.addEventListener('click', function(e){
+            console.log(`Moving to page: ${e.target.dataset.slug}`);
+            recordChoice(e.target.dataset.slug);
+            currentPage = getCurrentPage(e.target.dataset.slug);
+            updatePage(currentPage);
+            addEventListeners();
+        })
+    }
+}
+
+let undo = document.querySelector('#undo');
+undo.addEventListener('click', function(e){
+    console.log('Undoing last choice.');
+    undoLastChoice();
+    currentPage = getCurrentPage(choiceList[choiceList.length - 1]);
+    updatePage(currentPage);
+})
+
+currentPage = storyData.p1;
+updatePage(currentPage);
+addEventListeners();
+
